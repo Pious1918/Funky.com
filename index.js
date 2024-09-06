aceac5443b3eeff9807fecb38cdce4ac053f3c18
N S Pious <nspious1999@gmail.com>
2024-09-06 15:55:23 +0530

    cl_five

--- index.js
+++ index.js
@@ -9,6 +9,17 @@ const userRoute = require("./routes/userRoute");
 const path = require("path");
 app.use(express.json());
 // app.use(morgan("dev")); 
+const mongoose = require("mongoose");
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(() => {
+  console.log("Connected to MongoDB");
+})
+.catch((err) => {
+  console.error("MongoDB connection error:", err);
+});
 
 app.use(cookieParser());
 console.log("ppp",process.env.EMAIL) 
@@ -88,6 +99,7 @@ app.get("*", (req, res, next) => {
   } else {
     res.render("error");
   }
+
 });
 
 
